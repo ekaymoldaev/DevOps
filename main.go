9a225bf83ce2857da70f17396e7ffab29af1ccb7
ekaymoldaev <ekaymoldaev@gmail.com>
2023-04-06 06:00:55 +0600

    main.go on localhost

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	//устанавливаем роутер, то есть, роут “/”
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	http.ListenAndServe(":80", nil)
 }
